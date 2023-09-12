ff70e2bafe50d7d51777afe0244d7cdbacd841ba
django <django.ppp@gmail.com>
2023-09-12 19:32:27 +0300

    Добавлена регистрация и авторизация пользователей

--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from ckeditor_uploader.fields import RichTextUploadingField # type: ignore
+from django.utils.text import slugify
 
-# Create your models here.
+class Post(models.Model):
+    h1 = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    url = models.SlugField(unique=True)
+    description = RichTextUploadingField()
+    content = RichTextUploadingField()
+    image = models.ImageField()
+    created_at = models.DateField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=200)
+
+    
+    def __str__(self):
+        return self.title
+"""
+class Person(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10, blank=True)
+"""
--- app/views.py
+++ app/views.py
@@ -1,7 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+from .models import Post
+from django.core.paginator import Paginator
+
+from .forms import SigUpForm, SignInForm
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponseRedirect
 
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'app/home.html')
+        posts = Post.objects.all() # type: ignore
+        paginator = Paginator(posts, 6)
+        
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, 'app/home.html', context={
+            'page_obj': page_obj,
+        })
+
+class PostDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+
+        return render(request, 'app/post_detail.html', context={
+            'post': post,
+        })
+
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        form = SigUpForm()
+        return render(request, 'app/signup.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SigUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'app/signup.html', context={
+            'form': form,
+        })
+
+class SignInView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignInForm()
+        return render(request, 'app/signin.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'app/signin.html', context={
+            'form': form,
+        })
